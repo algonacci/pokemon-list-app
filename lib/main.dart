912acd24ac359909cdad16c4fa45206ef5e170bc
Eric Julianto <algonacci1@gmail.com>
2023-02-07 18:54:31 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,53 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
+  // @override
+  // void initState() {
+  //   super.initState();
+
+  //   fetchData();
+  // }
+
+  Future<List<dynamic>> fetchData() async {
+    var res = await http.get(Uri.parse(url));
+    var decodedJson = jsonDecode(res.body);
+    print(decodedJson['pokemon'][0]);
+    return decodedJson['pokemon'][0];
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.cyan,
-        colorScheme: const ColorScheme.light().copyWith(
-          primary: Colors.cyan,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: FutureBuilder<List<dynamic>>(
+          future: fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.length.toString());
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
         ),
       ),
-      home: Container(),
     );
   }
 }
